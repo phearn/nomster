5871d9051dc35a22b4378f75bb30b287319f4af7
Philip Hearn <zero.zain@gmail.com>
2016-11-15 09:32:19 +0000

    Added pagination to app

--- Gemfile
+++ Gemfile
@@ -41,3 +41,4 @@ gem 'spring',        group: :development
 gem 'rails_12factor', group: :production
 gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
+gem 'will_paginate', '~> 3.1.0'
---
+++ add10nomsterplaces.rb
@@ -0,0 +1,19 @@
+ Place.create(name: 'Marks Pizza', address: '345 Bark Street, Cambridge, MA 02139', description: 'One of the best Pizza places. If you can stomach seeing all the rats!')
+
+ Place.create(name: 'Veggie Hut', address: '324 Carrot Avenue, Cambridge, MA 02139', description: 'This place definetly has vegetables galore but little to offer in the way of flesh.')
+
+ Place.create(name: 'Dr. Taco', address: '664 Stratton Drive, Cambridge, MA 02139', description: 'Clinical grade taco ingredients wrapped in prescription pads!')
+
+ Place.create(name: 'Soda Fountain 2', address: '634 Trees Park, Cambridge, MA 02139', description: 'Lots of different sodas. Too bad what happened to Soda Fountain 1...')
+
+ Place.create(name: 'Chocolate Drinks', address: '324 Park Plaza, Cambridge, MA 02139', description: 'Chocolate drinks of a wide variety. They even let you use their freezer if you prefer to eat chocolate.')
+
+ Place.create(name: 'Human Brand Meats', address: '666 Happy Lane, Cambridge, MA 02139', description: 'Wonderful variety of deli meats. Not to be confused with the adjacent property Human Meats Brand.')
+
+ Place.create(name: 'Oodles of Noodles', address: '253 Longbread Street, Cambridge, MA 02139', description: 'Lots of different noodle dishes. Actually everything you order, including drinks, comes with noodles dumped in.')
+
+ Place.create(name: 'Hunt Ur Own', address: '120 Berkely Farm Road, Cambridge, MA 02139', description: 'Bring your own weapon and have at it with the local fauna. Cooksite is provided at location.')
+
+ Place.create(name: 'Whoops Salted Foods Bakery', address: '745 Blue Street, Cambridge, MA 02139', description: 'They meant to add sugar but now you get salty cake.')
+
+ Place.create(name: 'Smoothie Barn', address: '234 Anderson Lane, Cambridge, MA 02139', description: 'Bring whatever you want to this do-it-yourself smoothie facility. No judgments, no questions.')
