5023015187ec5640cb6d24a00f60e3f9889073cb
Philip Hearn <zero.zain@gmail.com>
2016-12-10 09:52:36 +0000

    Lesson 39. Challenge. Added ability to upload user images to places and connected heroku with AWS server.

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,6 @@ Rails.application.routes.draw do
   root 'places#index'
 
   resources :places do
-    resources :comments, only: :create
+    resources :comments, :photos, only: :create
   end
 end
